122256a5b694891055f63aa8b7e14f880c8d6495
Satria Arya Diva <121672225+satriaaryadiva@users.noreply.github.com>
2026-05-12 00:15:20 +0700

    feat: add Providers component with next-themes configuration

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -5,7 +5,7 @@ import * as React from 'react';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute='class' defaultTheme='light' enableSystem={false}>
+    <ThemeProvider attribute='class' defaultTheme='dark' enableSystem={false}>
       {children}
     </ThemeProvider>
   );
